9c375edac8c7a81a51ff1583885ebfe10491645b
John Källén <johnkal@gmail.com>
2023-12-02 00:15:43 +0100

    Fix: work on regressions
    AND and OR are used to bit-twiddle quantities that aren't necessarily uints, so don't assume that they are in ExpressionTypeDescender.
    
    "Soften" the result of a pointer difference to Domain.Integer (rather than Domain.SignedInt)
    
    SegmentedPtr and Pointer should unify if they're the same size.

--- src/Core/Types/Unifier.cs
+++ src/Core/Types/Unifier.cs
@@ -214,7 +214,7 @@ namespace Reko.Core.Types
             }
             if (b is PrimitiveType pb)
             {
-                if ((pb.Domain & (Domain.Selector | Domain.Pointer)) != 0 &&
+                if ((pb.Domain & (Domain.Selector | Domain.Pointer | Domain.SegPointer)) != 0 &&
                      pb.Size == ptrA.Size)
                     return true;
             }
@@ -782,7 +782,7 @@ namespace Reko.Core.Types
             if (b is PrimitiveType pb)
             {
                 if ((ptrA.Size == 0 || pb.Size == 0 || ptrA.Size == pb.Size) &&
-                    (pb.Domain & Domain.Pointer | Domain.Selector) != 0)
+                    (pb.Domain & (Domain.Pointer | Domain.SegPointer | Domain.Selector)) != 0)
                 {
                     return ptrA.Clone();
                 }
